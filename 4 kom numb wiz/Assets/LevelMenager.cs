22a0e56172adcda1b85f0ac3642fb5c825730fbe
kmlm <dunkierka2017@wp.pl>
2019-05-21 08:42:02 +0200

    add exit and some code

---
+++ 4 kom numb wiz/Assets/LevelMenager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMenager : MonoBehaviour;
+  {
+    void Update()
+    {
+        void LoadLevel(string name);
+        Debug.Log("Wczytano poziom" + name);
+    }
+}
